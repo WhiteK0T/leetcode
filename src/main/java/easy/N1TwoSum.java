9faf694607cefb86ef10b38cbe45665019631eab
WhiteK0T <alekssavenkov34@gmail.com>
2022-06-17 23:02:36 +0400

    add 1. Two Sum

---
+++ src/main/java/easy/N1TwoSum.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.HashMap;
+
+public class N1TwoSum {
+    /*
+        1. Two Sum
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+        Constraints:
+
+        2 <= nums.length <= 10 в 4 степени
+        -10 в 9 степени <= nums[i] <= 10 в 9 степени
+        -10 в 9 степени <= target <= 10 в 9 степени
+        Only one valid answer exists.
+
+        Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        Integer numberFromMap;
+        for (int i = 0; i < nums.length; i++) {
+            int requiredNumber = target - nums[i];
+            numberFromMap = map.get(requiredNumber);
+            if (numberFromMap != null && numberFromMap != i) {
+                return new int[]{i, map.get(requiredNumber)};
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ src/test/java/easy/N1TwoSumTest.java
@@ -0,0 +1,63 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class N1TwoSumTest {
+
+    N1TwoSum twoSum = new N1TwoSum();
+
+    /*
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
+    @Test
+    void twoSum1() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, actual);
+    }
+
+    /*
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+     */
+    @Test
+    void twoSum2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, actual);
+    }
+
+    /*
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
+     */
+    @Test
+    void twoSum3() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, actual);
+    }
+
+    /*
+        [2,5,5,11]
+        10
+     */
+    @Test
+    void twoSum4() {
+        int[] nums = {2, 5, 5, 11};
+        int target = 10;
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, actual);
+    }
+}
