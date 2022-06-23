e017e32c932336a28f57e3bd47a090d791c1cb80
WhiteK0T <alekssavenkov34@gmail.com>
2022-06-23 18:38:44 +0400

    Add N630. Course Schedule III

---
+++ src/main/java/hard/N630CourseScheduleIII.java
@@ -0,0 +1,40 @@
+package hard;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class N630CourseScheduleIII {
+    /*
+        630. Course Schedule III
+        There are n different online courses numbered from 1 to n. You are given an array courses where
+        courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi
+        days and must be finished before or on lastDayi.
+        You will start on the 1st day and you cannot take two or more courses simultaneously.
+
+        Return the maximum number of courses that you can take.
+
+        Constraints:
+
+            1 <= courses.length <= 104
+            1 <= durationi, lastDayi <= 104
+
+        https://leetcode.com/problems/course-schedule-iii/
+     */
+
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
+        PriorityQueue < Integer > queue = new PriorityQueue < > ((a, b) -> b - a);
+        int time = 0;
+        for (int[] c: courses) {
+            if (time + c[0] <= c[1]) {
+                queue.offer(c[0]);
+                time += c[0];
+            } else if (!queue.isEmpty() && queue.peek() > c[0]) {
+                time += c[0] - queue.poll();
+                queue.offer(c[0]);
+            }
+        }
+        return queue.size();
+    }
+}
---
+++ src/test/java/hard/N630CourseScheduleIIITest.java
@@ -0,0 +1,52 @@
+package hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class N630CourseScheduleIIITest {
+
+    N630CourseScheduleIII courseScheduleIII = new N630CourseScheduleIII();
+
+    /*
+        Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+        Output: 3
+        Explanation:
+        There are totally 4 courses, but you can take 3 courses at most:
+        First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
+        Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day.
+        Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day.
+        The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
+     */
+    @Test
+    void scheduleCourse1() {
+        int[][] courses = {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
+        int actual = courseScheduleIII.scheduleCourse(courses);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    /*
+        Input: courses = [[1,2]]
+        Output: 1
+    */
+    @Test
+    void scheduleCourse2() {
+        int[][] courses = {{1, 2}};
+        int actual = courseScheduleIII.scheduleCourse(courses);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    /*
+        Input: courses = [[3,2],[4,3]]
+        Output: 0
+    */
+    @Test
+    void scheduleCourse3() {
+        int[][] courses = {{3, 2}, {4, 3}};
+        int actual = courseScheduleIII.scheduleCourse(courses);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
