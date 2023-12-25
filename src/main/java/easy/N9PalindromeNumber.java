528ff7c84042c94bfe041bd0f0a10f9c14109c77
SAM <xiryrg-777@yandex.ru>
2023-12-26 01:27:54 +0400

    Add 9. Palindrome Number

---
+++ src/main/java/easy/N9PalindromeNumber.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class N9PalindromeNumber {
+    /*
+        9. Palindrome Number
+
+        Given an integer x, return true if x is a palindrome , and false otherwise.
+
+        Constraints:
+        -231 <= x <= 231 - 1
+
+        Follow up: Could you solve it without converting the integer to a string?
+     */
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int reverse = 0;
+        int temp = x;
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+        return (reverse == x);
+    }
+}
---
+++ src/test/java/easy/N9PalindromeNumberTest.java
@@ -0,0 +1,48 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
+class N9PalindromeNumberTest {
+
+    public static final N9PalindromeNumber PALINDROME_NUMBER = new N9PalindromeNumber();
+
+    /*  Example 1:
+        Input: x = 121
+        Output: true
+        Explanation: 121 reads as 121 from left to right and from right to left.     */
+    @Test
+    void isPalindrome1() {
+        int input = 121;
+        boolean expected = true;
+        boolean actual = PALINDROME_NUMBER.isPalindrome(input);
+        assertThat(actual, equalTo(expected));
+    }
+
+    /*  Example 2:
+        Input: x = -121
+        Output: false
+        Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.     */
+    @Test
+    void isPalindrome2() {
+        int input = -121;
+        boolean expected = false;
+        boolean actual = PALINDROME_NUMBER.isPalindrome(input);
+        assertThat(actual, equalTo(expected));
+    }
+
+    /*  Example 3:
+        Input: x = 10
+        Output: false
+        Explanation: Reads 01 from right to left. Therefore it is not a palindrome.     */
+    @Test
+    void isPalindrome3() {
+        int input = 10;
+        boolean expected = false;
+        boolean actual = PALINDROME_NUMBER.isPalindrome(input);
+        assertThat(actual, equalTo(expected));
+    }
+}
